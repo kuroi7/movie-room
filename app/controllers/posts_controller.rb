8645f26c21f5da658ecb1ee4e5357e96196885e9
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-04-13 11:10:40 +0900

    review function
    レビュー投稿機能を実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.includes(:user)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content).merge(user_id: current_user.id)
+  end
+end
