381f549c5e37bc2da30381afbcdcd8c5723aa14b
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-04-15 12:15:24 +0900

    commit initial files
    初期ファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "users#show"
-  resources :users, only: [:edit, :update, :show]
+  resources :users, only: [:index, :edit, :update, :show]
   resources :messages, only: [:index]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   resources :my_rankings do
     put :sort
   end
+  resources :relationships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200414072920) do
+ActiveRecord::Schema.define(version: 20200414133310) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -54,6 +54,16 @@ ActiveRecord::Schema.define(version: 20200414072920) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "user_id"
+  end
+
+  create_table "relationships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "follower_id"
+    t.integer  "following_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true, using: :btree
+    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
+    t.index ["following_id"], name: "index_relationships_on_following_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
